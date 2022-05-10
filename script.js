f2fb923631e9976b51139c7b3726a20770b9b36b
Lee Tran <lee.tran95@yahoo.com>
2022-05-09 20:28:18 -0700

    Create a function that takes in playerselection and computer selection to determine the winnenr

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 function computerPlay(){
     let random = Math.ceil(Math.random()*3);
     if(random === 1){
-        return 'Rock';
+        return 'rock';
     }else if(random === 2){
-        return 'Scissors'
+        return 'scissors'
     }else if(random === 3){
-        return 'Paper'
+        return 'paper'
     }
     return null
 }
 const computerSelection = computerPlay();
 const playerSelection = prompt("Please enter rock, paper, or scissors").toLowerCase();
 
+function playRound(computerSelection, playerSelection){
+    if(playerSelection === computerSelection){
+        return "It's a draw!"
+    }
+    if(playerSelection === 'rock'){
+        return computerSelection === 'scissors' ? "You Win! Rock beats Scissors":"You Lose! Paper beats Rock"
+    }else if(playerSelection === 'paper'){
+        return computerSelection === 'rock' ? "You Win! Paper beats Rock":"You Lose! Scissors beats Paper"
+    }else if(playerSelection === 'scissors'){
+        return computerSelection === 'paper' ? "You win! Scissors beats Paper":"You Lose! Rock beats Scissors"
+    }
+}
+
+console.log(playRound(computerSelection, playerSelection))
